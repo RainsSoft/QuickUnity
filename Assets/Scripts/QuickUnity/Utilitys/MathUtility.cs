922fdf927af2168dcede1037c2d064e2a299dc34
Jerry Lee <cosmos53076@163.com>
2014-11-07 23:58:16 +0800

    add method IsOdd to MathUtility

--- Assets/Scripts/QuickUnity/Utilitys/MathUtility.cs
+++ Assets/Scripts/QuickUnity/Utilitys/MathUtility.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// The Utilitys namespace.
@@ -42,13 +43,33 @@ namespace QuickUnity.Utilitys
         /// <returns>System.Single.</returns>
         public static float GenGaussianRnd()
         {
-            float x1 = Random.value;
-            float x2 = Random.value;
+            float x1 = UnityEngine.Random.value;
+            float x2 = UnityEngine.Random.value;
 
             if (x1 == 0.0f)
                 x1 = 0.01f;
 
             return (float)(System.Math.Sqrt(-2.0 * System.Math.Log(x1)) * System.Math.Cos(2.0 * Mathf.PI * x2));
+        }
+
+        /// <summary>
+        /// Determines whether the specified n is odd.
+        /// </summary>
+        /// <param name="n">The n.</param>
+        /// <returns><c>true</c> if the specified n is odd; otherwise, <c>false</c>.</returns>
+        public static bool IsOdd(int n)
+        {
+            return Convert.ToBoolean(n & 1);
+        }
+
+        /// <summary>
+        /// Determines whether the specified n is odd.
+        /// </summary>
+        /// <param name="n">The n.</param>
+        /// <returns><c>true</c> if the specified n is odd; otherwise, <c>false</c>.</returns>
+        public static bool IsOdd(float n)
+        {
+            return Convert.ToBoolean(Mathf.FloorToInt(n) & 1);
         }
     }
 }
